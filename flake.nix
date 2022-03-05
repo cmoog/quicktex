0ed915d1a654fab9fa0eabf3e51537d7e9c9d14f
Charlie Moog <moogcharlie@gmail.com>
2022-03-05 15:23:27 -0600

    dev: add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My personal website served from cmoog.io and charliemoog.com.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ nodejs-16_x ];
+        };
+      }
+    );
+}
