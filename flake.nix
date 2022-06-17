81f71cd39a2a61aad42babef8da4a6046d406c57
Charlie Moog <moogcharlie@gmail.com>
2022-06-17 09:24:33 -0500

    npm audit fix

--- flake.nix
+++ flake.nix
@@ -7,12 +7,10 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
+      let pkgs = nixpkgs.legacyPackages.${system}; in
       {
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [ nodejs-16_x deno ];
+        devShells.default = with pkgs; mkShell {
+          packages = [ nodejs-16_x deno ];
         };
       }
     );
